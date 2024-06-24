70448acc93a81184ccd474698df82a04ff40a343
Mina <mina@artifectai.com>
2024-06-25 00:34:41 +0100

    Refactored Schedule Folder and Updated Tests

--- src/common/schedule/schedule.module.ts
+++ src/common/schedule/schedule.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
-import { AccessRequestsModule } from '../modules/access-requests/access-requests.module';
+import { AccessRequestsModule } from '../../modules/access-requests/access-requests.module';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
-import { DatasetsModule } from '../modules/datasets/datasets.module';
+import { DatasetsModule } from '../../modules/datasets/datasets.module';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { Frequency } from '../models/frequency.model';
+import { Frequency } from '../../models/frequency.model';
 
 @Module({
   imports: [
